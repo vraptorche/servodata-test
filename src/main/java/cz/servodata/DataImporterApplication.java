b27b91f36b2410f0aeb171dd82020b2c4f2ed7e9
vraptor <emil.korladinov@gmail.com>
2016-12-08 08:49:31 +0100

    CommandRunner -WIP

--- src/main/java/cz/servodata/DataImporterApplication.java
+++ src/main/java/cz/servodata/DataImporterApplication.java
@@ -1,12 +1,18 @@
 package cz.servodata;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DataImporterApplication {
+public class DataImporterApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataImporterApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
 	}
 }
--- src/main/java/cz/servodata/config/DataImporterConfig.java
+++ src/main/java/cz/servodata/config/DataImporterConfig.java
@@ -1,8 +1,13 @@
 package cz.servodata.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import cz.servodata.core.domain.Persistent;
 import cz.servodata.core.repository.CompanyRepository;
+import cz.servodata.core.service.CsvImportService;
+import cz.servodata.core.service.DefaultCsvImportService;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,4 +15,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackageClasses = Persistent.class)
 @EnableJpaRepositories(basePackageClasses = CompanyRepository.class)
 public class DataImporterConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper().disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+    }
+
+    @Bean
+    public CsvImportService csvImportService() {
+        return new DefaultCsvImportService();
+    }
 }
